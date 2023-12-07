52909948330705d752a60a307f3c16728490d139
dhruvkapadi <dhruv.kapadi@seaflux.tech>
2023-12-07 18:49:44 +0000

    feat : add / route

--- index.js
+++ index.js
@@ -15,6 +15,11 @@ app.use(cors());
 const port = process.env.PORT || 3000;
 
 // Your routes remain unchanged
+app.get("/", async (req, res) => {
+    return res.status(200).json({ message : "this is a log" });
+});
+
+
 app.get("/add", async (req, res) => {
     const { num1, num2 } = req.query;
     return res.status(200).json({ result: parseFloat(num1) + parseFloat(num2) });
