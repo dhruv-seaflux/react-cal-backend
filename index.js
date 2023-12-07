ba9c8d14aa45022ec10f2bc85aa336d29383ea8e
dhruvkapadi <dhruv.kapadi@seaflux.tech>
2023-12-07 18:43:13 +0000

    inital commit

---
+++ index.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const cors = require("cors"); // Import the 'cors' middleware
+const app = express();
+require("dotenv").config();
+
+const jsonParser = express.json(); // Middleware for JSON parsing
+const urlEncodedParser = express.urlencoded({ extended: true }); // Middleware for URL-encoded data parsing
+
+app.use(jsonParser);
+app.use(urlEncodedParser);
+
+// Enable CORS for all routes
+app.use(cors());
+
+const port = process.env.PORT || 3000;
+
+// Your routes remain unchanged
+app.get("/add", async (req, res) => {
+    const { num1, num2 } = req.query;
+    return res.status(200).json({ result: parseFloat(num1) + parseFloat(num2) });
+});
+
+app.get("/sub", async (req, res) => {
+    const { num1, num2 } = req.query;
+    return res.status(200).json({ result: parseFloat(num1) - parseFloat(num2) });
+});
+
+app.get("/mul", async (req, res) => {
+    const { num1, num2 } = req.query;
+    return res.status(200).json({ result: parseFloat(num1) * parseFloat(num2) });
+});
+
+app.get("/div", async (req, res) => {
+    const { num1, num2 } = req.query;
+    return res.status(200).json({ result: parseFloat(num1) / parseFloat(num2) });
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port: ${port}`);
+});
